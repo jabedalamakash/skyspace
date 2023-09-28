b5b43f1f5ca9c1578389f65b76fb51b3dedd7640
jabedalamakash <jabedalamakash00@gmail.com>
2023-09-28 12:19:30 +0600

    toogle

--- components/Mission.jsx
+++ components/Mission.jsx
@@ -28,24 +28,26 @@ const Mission = ({data}) => {
                     
                     <td className='p-5 font-bold border'>{item.mission_name}</td>
                     <td className='p-5 border'>{item.description}</td>
-                    {status?(<td className='p-5 border '> <button className='text-white text-sm bg-red-600 px-3 py-2 rounded-md'>NOT A MEMBER</button></td>)
-                    :(<td className='p-5 border'><button className=' text-white text-sm  bg-green-600 px-3 py-2 rounded-md '>ACTIVE MEMBER</button></td>)}
-                    
-                    {/* <button className='p-5 border'>Join Member</button> */}
+                    {status?(<td className='p-5 border '> <button
+                    className='text-white text-sm bg-red-600 px-3 py-2 rounded-md'>NOT A MEMBER</button></td>)
+                    :(<td className='p-5 border'><button
+                     className=' text-white text-sm  bg-green-600 px-3 py-2 rounded-md '>ACTIVE MEMBER</button></td>)}
+
                     <td  className='p-5 border' >
                     {status ? (<button onClick={()=>{
                     setStatus(false);
-                    setMember("ACTIVE MEMBER");   
-                    handlemissionR(item);
-                
-                }} className='bg-blue-600  border-blue-600 rounded-md text-sm  text-white px-2 py-2'>Join Mission</button>)
-                :(<button onClick={()=>{
+                    handlemissionR(item);}}
+                    className='bg-blue-600  border-blue-600 rounded-md text-sm  text-white px-2 py-2'>Join Mission</button>)
+                    :(<button onClick={()=>{
                     setStatus(true);
-                 setMember("NOT A MEMBER");
-                missionDelete(item.mission_id);
-               
-               }} 
-                className='text-white  bg-red-600 text-sm rounded-md px-2 py-2'>Leave Mission</button>)}
+                    missionDelete(item.mission_id);}}
+                    className='text-white  bg-red-600 text-sm rounded-md px-2 py-2'>Leave Mission</button>)}
+
+                    
+
+
+
+
                 </td>
                 </tr>
             })}
